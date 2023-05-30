ffc9eda518264504a7a936010554e81a91ff3e4c
Sergei <blackfoor3@gmail.com>
2023-05-30 16:16:21 +0300

    123

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(function(){
+
+    $('.slider-blog__inner').slick({
+    dots:true,
+
+    });
+
+    $('.menu__btn').on('click', function(){
+        $('.header__top-inner').toggleClass('.header__top-inner--active');
+    });
+
     var mixer = mixitup('.portfolio__content')
 });
